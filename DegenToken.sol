3e41f78de155eb03449da8b77f3104a2ef039836
priyansh06rawat <priyanshrawat122004@gmail.com>
2024-08-10 14:33:20 +0530

    Update DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -4,39 +4,55 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract DegenGamingToken is ERC20, Ownable {
 
-    event TokensRedeemed(address indexed user, uint amount, string itemName);
+    event TokensRedeemed(address indexed user, uint256 amount, string itemName);
 
+    // Mapping to store item prices
     mapping(string => uint256) public itemPrices;
 
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {}
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+        
+        itemPrices["GTA"] = 100;                      
+        itemPrices["PUBG"] = 70;
+        itemPrices["Free Fire"] = 50;
+        itemPrices["COC"] = 50;
+        itemPrices["Zombie dash"] = 40;
+        itemPrices["Shoot"] = 30;
+    }
 
+    
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
+    
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
 
-    function transferToken(address _toAddress, uint _amount) public {
-        bool success = transfer(_toAddress, _amount);
-        assert(success);  
+    
+    function transferToken(address to, uint256 amount) public {
+        transfer(to, amount);
     }
 
-    // Function to set the price of an item
+   
     function setItemPrice(string memory itemName, uint256 price) public onlyOwner {
         itemPrices[itemName] = price;
     }
 
-    // Function to redeem an item
-    function redeemItem(string memory itemName) public {
-        uint256 price = itemPrices[itemName];
-        require(price > 0, "Item does not exist");
-        require(balanceOf(msg.sender) >= price, "Insufficient token balance to redeem");
+    // Function to redeem tokens for an item
+    function redeemToken(string memory itemName) public {
+        uint256 amount = itemPrices[itemName];
+        require(amount > 0, "Item does not exist");
+        require(balanceOf(msg.sender) >= amount, "Insufficient token balance to redeem");
+        _burn(msg.sender, amount);
+        
+        
+        emit TokensRedeemed(msg.sender, amount, itemName);
+    }
 
-        _burn(msg.sender, price);
-        emit TokensRedeemed(msg.sender, price, itemName);
+    function checkTokenBalance(address account) public view returns (uint256) {
+        return balanceOf(account);
     }
 }
